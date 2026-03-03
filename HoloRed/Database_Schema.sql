956f026b048a74dca700b868d6bb37ff32646fb4
Adrián Dondarza <adriandondarza@gmail.com>
2026-03-03 11:57:59 +0100

    Archivo con los script de las tablas de las bases de datos y como acceder a ellas

--- HoloRed/Database_Schema.sql
+++ HoloRed/Database_Schema.sql
@@ -1,3 +1,5 @@
+// SCRIPT PARA CREAR LA BASE DE DATOS DE CASSANDRA
+
 CREATE KEYSPACE IF NOT EXISTS holored 
 WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
 
@@ -13,22 +15,46 @@ CREATE TABLE IF NOT EXISTS telemetria_combate (
     PRIMARY KEY ((sector_id), fecha, timestamp)
 ) WITH CLUSTERING ORDER BY (fecha DESC, timestamp DESC);NG ORDER BY (fecha DESC, timestamp DESC);
 
--- 1. Creación del Keyspace (Base de Datos)
+//Para acceder:
+docker exec -it cassandra_telemetria cqlsh -u admin -p RepublicBattle_2026!
 
-CREATE KEYSPACE IF NOT EXISTS holored 
-WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
 
--- 2. Selección del Keyspace
-USE holored;
 
--- 3. Creación de la Tabla de Impactos
--- Nota: Se utiliza 'DanoEscudos' sin caracteres especiales (ñ) para evitar errores de sintaxis
-CREATE TABLE IF NOT EXISTS impactos (
-    SectorId text,
-    Fecha date,
-    Timestamp timestamp,
-    NaveAtacante text,
-    NaveObjetivo text,
-    DanoEscudos int,
-    PRIMARY KEY ((SectorId, Fecha), Timestamp)
-) WITH CLUSTERING ORDER BY (Timestamp DESC);
+
+// SCRIPT PARA CREAR LA BASE DE DATOS DE NEO4J
+
+// Limpiamos datos anteriores para evitar duplicados
+MATCH (n) DETACH DELETE n;
+
+// Creamos las Facciones
+CREATE (nr:Faccion {nombre: 'Nueva Republica'})
+CREATE (ri:Faccion {nombre: 'Remanente Imperial'})
+
+// Creamos a Kallus y su red de traición
+CREATE (k:Espia {nombre: 'Kallus'})
+
+// RELACIÓN OFICIAL: Kallus finge estar en la Nueva República
+CREATE (k)-[:INFILTRADO_EN]->(nr)
+
+// RELACIÓN OCULTA: Kallus realmente ayuda al Imperio (esto lo delata)
+CREATE (k)-[:SUMINISTRA_ARMAS_A]->(ri)
+CREATE (k)-[:INFORMA_MOVIMIENTOS_A]->(ri)
+
+// Creamos un espía LEAL para comparar (Andor no debería salir)
+CREATE (a:Espia {nombre: 'Andor'})
+CREATE (a)-[:INFILTRADO_EN]->(ri)
+CREATE (a)-[:SABOTEA_OPERACIONES_DE]->(ri)USTERING ORDER BY (Timestamp DESC);
+
+//Para acceder:
+docker exec -it neo4j_espionaje cypher-shell -u neo4j -p RepublicSpies_2026!
+
+
+
+// Redis funciona como un almacén de Llave-Valor (Key-Value Store).
+Comprobacion de 10 minuntos TTL:
+docker exec -it redis_radar redis-cli -a RepublicRadar_2026!
+2) KEYS * (Para ver el nombre de la clave, ej: "nave:XWING-01").
+3) TTL "nave:XWING-01" (Debe devolver un número menor a 600 segundos
+
+//Para acceder:
+docker exec -it redis_radar redis-cli -a RepublicRadar_2026!
