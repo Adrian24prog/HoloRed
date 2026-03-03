533e373f6d038153b857ef1311273dfba641dcc2
naraanjo <185625172+naraanjo@users.noreply.github.com>
2026-03-03 11:40:23 +0100

    Acrtualizacion de los esquermas

--- HoloRed/Database_Schema.sql
+++ HoloRed/Database_Schema.sql
@@ -12,3 +12,23 @@ CREATE TABLE IF NOT EXISTS telemetria_combate (
     danio_escudos int,
     PRIMARY KEY ((sector_id), fecha, timestamp)
 ) WITH CLUSTERING ORDER BY (fecha DESC, timestamp DESC);NG ORDER BY (fecha DESC, timestamp DESC);
+
+-- 1. Creación del Keyspace (Base de Datos)
+
+CREATE KEYSPACE IF NOT EXISTS holored 
+WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
+
+-- 2. Selección del Keyspace
+USE holored;
+
+-- 3. Creación de la Tabla de Impactos
+-- Nota: Se utiliza 'DanoEscudos' sin caracteres especiales (ñ) para evitar errores de sintaxis
+CREATE TABLE IF NOT EXISTS impactos (
+    SectorId text,
+    Fecha date,
+    Timestamp timestamp,
+    NaveAtacante text,
+    NaveObjetivo text,
+    DanoEscudos int,
+    PRIMARY KEY ((SectorId, Fecha), Timestamp)
+) WITH CLUSTERING ORDER BY (Timestamp DESC);
